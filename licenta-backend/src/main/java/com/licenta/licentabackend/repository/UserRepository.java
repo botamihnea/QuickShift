d1fe96c29e0a513b3111e5a57a6ac48df2e77711
botamihnea <mihneabota2004@gmail.com>
2026-04-21 11:15:02 +0300

    Fixed missing annotation is UserRepo

--- licenta-backend/src/main/java/com/licenta/licentabackend/repository/UserRepository.java
+++ licenta-backend/src/main/java/com/licenta/licentabackend/repository/UserRepository.java
@@ -2,8 +2,11 @@ package com.licenta.licentabackend.repository;
 
 import com.licenta.licentabackend.domain.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<AppUser, Long> {
     Optional<AppUser> findByEmail(String email);
 }
